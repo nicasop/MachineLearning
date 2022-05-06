806695d8efca57e14ad347c0e199d714159d5832
nicasop <nicolas_villita21@hotmail.es>
2022-05-06 16:42:41 -0500

    final deber5

--- deber5.py
+++ deber5.py
@@ -7,60 +7,77 @@ import tdIdf as tdf
 import cosenoVectorial as cosV
 
 #FUNCIONES
-# def matrizDistanciaPonderada(matriz1,matriz2,matriz3,ponderacines):
-#     i = j = 0
-#     while True:
-
-
-
+def matrizDistanciaPonderada(matriz1,matriz2,matriz3,ponderacines):
+    matrizDistancias = np.zeros((len(matriz1),len(matriz1)))
+    print(matrizDistancias)
+    i = j = 0
+    while True:
+        matrizDistancias[i][j] = matriz1[i][j]*ponderacines[0] + matriz2[i][j]*ponderacines[1] + matriz3[i][j]*ponderacines[2]
+        j += 1
+        if j ==  len(matriz1):
+            j = 0
+            i += 1
+        if i == len(matriz1):
+            break
+    return matrizDistancias
 
 ##IMPORTAR DATOS
+
 papers = nlp.importarCSV('papers.csv')
-# titulos = papers['title'].tolist()
+titulos = papers['title'].tolist()
 keywords = papers['keywords'].tolist()
-# abstracts = papers['abstract'].tolist()
+abstracts = papers['abstract'].tolist()
 
 ##NLP
-# titulosL = nlp.limpiarDocumento(titulos,'en')
+titulosL = nlp.limpiarDocumento(titulos,'en')
 keywordsL = nlp.limpiarDocumento(keywords,'en')
-print(keywordsL[len(keywordsL)-1])
-print(jaccard.interseccion(keywordsL[len(keywordsL)-1],keywordsL[len(keywordsL)-1]))
-print(jaccard.union(keywordsL[len(keywordsL)-1],keywordsL[len(keywordsL)-1]))
-valor = jaccard.jaccard(keywordsL[len(keywordsL)-1],keywordsL[len(keywordsL)-1])
-print(valor)
-# abstractsL = nlp.limpiarDocumento(abstracts,'en')
+# print(keywordsL[len(keywordsL)-1])
+# print(jaccard.indexarDoc(keywordsL[len(keywordsL)-1]))
+# print(jaccard.interseccion(keywordsL[len(keywordsL)-1],keywordsL[len(keywordsL)-1]))
+# print(jaccard.union(keywordsL[len(keywordsL)-1],keywordsL[len(keywordsL)-1]))
+# valor = jaccard.jaccard(keywordsL[len(keywordsL)-1],keywordsL[len(keywordsL)-1])
+# print(valor)
+abstractsL = nlp.limpiarDocumento(abstracts,'en')
 
 ##JACCARD
 #TITULOS
-# matrizTit = jaccard.matrizJaccard(titulosL)
+matrizTit = jaccard.matrizJaccard(titulosL)
+print(matrizTit)
 
 #KEYWORDS
 matrizKey = jaccard.matrizJaccard(keywordsL)
-print(matrizKey[len(matrizKey)-1][len(matrizKey)-1])
+print(matrizKey)
+# print(matrizKey[len(matrizKey)-1][len(matrizKey)-1])
 
 ##FULL INVERTED INDEX -- TF-IDF -- COSENO VECTORIAL
-# #FULL INVERTED INDEX
-# diccionario={'tokens':[],'ocurrencias':[]}
-# diccionario['tokens']= nlp.indexacionToken(abstractsL)
-# diccionario['ocurrencias'] = nlp.ocurrencias(diccionario['tokens'],abstractsL)
-# # nlp.imprimirFII(diccionario['tokens'],diccionario['ocurrencias'])
+#FULL INVERTED INDEX
+diccionario={'tokens':[],'ocurrencias':[]}
+diccionario['tokens']= nlp.indexacionToken(abstractsL)
+diccionario['ocurrencias'] = nlp.ocurrencias(diccionario['tokens'],abstractsL)
+# nlp.imprimirFII(diccionario['tokens'],diccionario['ocurrencias'])
 
-# #TF-IDF
-# matriz = tdf.bagWords(diccionario,abstractsL)
-# # print(matriz)
-# wtf = tdf.matrizPTF(matriz)
-# # print(wtf)
-# dF = tdf.documentF(wtf)
-# # print(dF)
-# idf = tdf.IDF(dF,len(abstractsL))
-# # print(idf)
-# tf_idf = tdf.TFIDF(wtf,idf)
-# # print(tf_idf)
+#TF-IDF
+matriz = tdf.bagWords(diccionario,abstractsL)
+# print(matriz)
+wtf = tdf.matrizPTF(matriz)
+# print(wtf)
+dF = tdf.documentF(wtf)
+# print(dF)
+idf = tdf.IDF(dF,len(abstractsL))
+# print(idf)
+tf_idf = tdf.TFIDF(wtf,idf)
+# print(tf_idf)
 
-# #Coseno Vectorial
-# matrizN = cosV.matrizNormal(tf_idf)
-# # print(matrizN)
-# # matrizDistanciaAbs = cosV.matrizDistacias(cosV.matrizNormal(tf_idf))
-# matrizAbs = cosV.matrizDistacias(matrizN)
-# print(matrizAbs)
-# ponderacion = [0.5,0.3,0.2]
+#Coseno Vectorial
+matrizN = cosV.matrizNormal(tf_idf)
+# print(matrizN)
+# matrizDistanciaAbs = cosV.matrizDistacias(cosV.matrizNormal(tf_idf))
+matrizAbs = cosV.matrizDistacias(matrizN)
+print(matrizAbs)
+
+#MATRIZ DE DISTACIAS
+ponderacion = [0.5,0.3,0.2]
+Distancias = matrizDistanciaPonderada(matrizAbs,matrizKey,matrizTit,ponderacion)
+print('Matriz de distancias\n',Distancias)
+
+
--- jaccard.py
+++ jaccard.py
@@ -3,10 +3,19 @@
 #### LIBRERIAS ####
 import numpy as np
 
+def indexarDoc(a):
+    aL = []
+    for element in a:
+        if element not in aL:
+            aL.append(element)
+    return aL
+
 def interseccion(a,b):
+    aL = indexarDoc(a)
+    bL = indexarDoc(b)
     cont=0
-    for token in a:
-        for token1 in b:
+    for token in aL:
+        for token1 in bL:
             if token == token1:
                 cont += 1
     return cont
